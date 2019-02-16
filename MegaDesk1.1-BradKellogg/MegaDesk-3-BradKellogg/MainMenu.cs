af555faf96e8d4de618fa4b394e39151f04d50c7
waabbajak <waabbajak@gmail.com>
2019-02-16 16:29:56 -0700

    Initial

---
+++ MegaDesk1.1-BradKellogg/MegaDesk-3-BradKellogg/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_3_BradKellogg
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void AddQuoteButton_Click(object sender, EventArgs e)
+        {
+            AddQuote addNewQuoteForm = new AddQuote();
+            addNewQuoteForm.Tag = this;
+            addNewQuoteForm.Show(this);
+            Hide();
+        }
+
+        private void ViewQuotesButton_Click(object sender, EventArgs e)
+        {
+            ViewAllQuotes viewAllQuotesForm = new ViewAllQuotes();
+            viewAllQuotesForm.Tag = this;
+            viewAllQuotesForm.Show(this);
+            Hide();
+        }
+
+        private void SearchQuotesButton_Click(object sender, EventArgs e)
+        {
+            SearchQuotes searchQuotesForm = new SearchQuotes();
+            searchQuotesForm.Tag = this;
+            searchQuotesForm.Show(this);
+            Hide();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ MegaDesk1.1-BradKellogg/MegaDesk-3-BradKellogg/SearchQuotes.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MegaDesk_3_BradKellogg
+{
+    partial class SearchQuotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SearchComboBox = new System.Windows.Forms.ComboBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ResultsBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BackButton
+            // 
+            this.BackButton.AutoSize = true;
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackButton.Location = new System.Drawing.Point(199, 495);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(120, 35);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // SearchComboBox
+            // 
+            this.SearchComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchComboBox.FormattingEnabled = true;
+            this.SearchComboBox.Location = new System.Drawing.Point(12, 12);
+            this.SearchComboBox.Name = "SearchComboBox";
+            this.SearchComboBox.Size = new System.Drawing.Size(166, 28);
+            this.SearchComboBox.TabIndex = 3;
+            this.SearchComboBox.SelectedIndexChanged += new System.EventHandler(this.SearchComboBox_SelectedIndexChanged);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(184, 11);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(135, 28);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ResultsBox
+            // 
+            this.ResultsBox.FormattingEnabled = true;
+            this.ResultsBox.Location = new System.Drawing.Point(13, 47);
+            this.ResultsBox.Name = "ResultsBox";
+            this.ResultsBox.Size = new System.Drawing.Size(306, 433);
+            this.ResultsBox.TabIndex = 5;
+            // 
+            // SearchQuotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 542);
+            this.Controls.Add(this.ResultsBox);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SearchComboBox);
+            this.Controls.Add(this.BackButton);
+            this.Name = "SearchQuotes";
+            this.Text = "MegaDesk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.ComboBox SearchComboBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ListBox ResultsBox;
+    }
+}
