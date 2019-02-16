20f6b2f34ea8e7df7cd33481740ed50a290cd69e
waabbajak <waabbajak@gmail.com>
2019-02-16 16:25:04 -0700

    Initial

---
+++ MegaDesk1.1-BradKellogg/MegaDesk-3-BradKellogg/Desk.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaDesk_3_BradKellogg
+{
+    enum Material {
+        Laminate = 100,
+        Oak = 200,
+        Rosewood = 300,
+        Veneer = 125,
+        Pine = 50 };
+
+    class Desk
+    {
+        public int Width;
+        public int Depth;
+        public int numDrawers;
+        public Material deskMaterial;
+
+        public Desk()
+        {
+
+        }
+
+        public Desk(int width, int depth, int numDrawers)
+        {
+            this.Width = width;
+            this.Depth = depth;
+            this.numDrawers = numDrawers;
+        }
+
+        public int getWidth()
+        {
+            return Width;
+        }
+
+        public int getDepth()
+        {
+            return Depth;
+        }
+
+        public int getNumDrawers()
+        {
+            return numDrawers;
+        }
+
+        public Material getDeskMaterial()
+        {
+            return deskMaterial;
+        }
+
+        public void setWidth(int width)
+        {
+            this.Width = width;
+        }
+
+        public void setDepth(int depth)
+        {
+            this.Depth = depth;
+        }
+
+        public void setNumDrawers(int number)
+        {
+            this.numDrawers = number;
+        }
+
+        public float calcDeskPrice()
+        {
+            float price = 200;
+
+            if (Width * Depth > 1000)
+            {
+                price += Width * Depth;
+            }
+
+            price += numDrawers * 50;
+
+            switch (deskMaterial) 
+            {
+                case Material.Oak:
+                    price += 200;
+                    break;
+                case Material.Laminate:
+                    price += 100;
+                    break;
+                case Material.Pine:
+                    price += 50;
+                    break;
+                case Material.Rosewood:  
+                    price += 300;
+                    break;
+                case Material.Veneer:
+                    price += 125;
+                    break;
+                default:
+                    Console.WriteLine("Invalid desk material");
+                    break;
+            }
+
+            return price;
+       }
+    }
+}
---
+++ MegaDesk1.1-BradKellogg/MegaDesk-3-BradKellogg/MainMenu.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MegaDesk_3_BradKellogg
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.addQuoteButton = new System.Windows.Forms.Button();
+            this.viewQuotesButton = new System.Windows.Forms.Button();
+            this.searchQuotesButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addQuoteButton
+            // 
+            this.addQuoteButton.AutoSize = true;
+            this.addQuoteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addQuoteButton.Location = new System.Drawing.Point(70, 53);
+            this.addQuoteButton.Name = "addQuoteButton";
+            this.addQuoteButton.Size = new System.Drawing.Size(161, 35);
+            this.addQuoteButton.TabIndex = 1;
+            this.addQuoteButton.Text = "Add New Quote";
+            this.addQuoteButton.UseVisualStyleBackColor = true;
+            this.addQuoteButton.Click += new System.EventHandler(this.AddQuoteButton_Click);
+            // 
+            // viewQuotesButton
+            // 
+            this.viewQuotesButton.AutoSize = true;
+            this.viewQuotesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewQuotesButton.Location = new System.Drawing.Point(70, 112);
+            this.viewQuotesButton.Name = "viewQuotesButton";
+            this.viewQuotesButton.Size = new System.Drawing.Size(161, 35);
+            this.viewQuotesButton.TabIndex = 2;
+            this.viewQuotesButton.Text = "View Quotes";
+            this.viewQuotesButton.UseVisualStyleBackColor = true;
+            this.viewQuotesButton.Click += new System.EventHandler(this.ViewQuotesButton_Click);
+            // 
+            // searchQuotesButton
+            // 
+            this.searchQuotesButton.AutoSize = true;
+            this.searchQuotesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchQuotesButton.Location = new System.Drawing.Point(70, 172);
+            this.searchQuotesButton.Name = "searchQuotesButton";
+            this.searchQuotesButton.Size = new System.Drawing.Size(161, 35);
+            this.searchQuotesButton.TabIndex = 3;
+            this.searchQuotesButton.Text = "Search Quotes";
+            this.searchQuotesButton.UseVisualStyleBackColor = true;
+            this.searchQuotesButton.Click += new System.EventHandler(this.SearchQuotesButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(370, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 221);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.AutoSize = true;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(70, 239);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(161, 35);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.searchQuotesButton);
+            this.Controls.Add(this.viewQuotesButton);
+            this.Controls.Add(this.addQuoteButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MegaDesk";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addQuoteButton;
+        private System.Windows.Forms.Button viewQuotesButton;
+        private System.Windows.Forms.Button searchQuotesButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
